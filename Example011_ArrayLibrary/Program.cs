1bb8e323f0756d73b4ff8371041408502f90acd3
Ecs <ecstrazzz@ya.ru>
2023-12-05 23:59:27 +0500

    add Example011_ArrayLibrary project, with my improvement

---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,57 @@
+﻿void FillArray(int[] collection)
+{
+    int Length = collection.Length;
+    int index = 0;
+    while (index < Length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        //index = index + 1;
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+
+Console.WriteLine("-------");
+Console.WriteLine("Введите число необходимого элемента: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("-------");
+
+int pos = IndexOf(array, num);
+if (pos!=-1)
+{
+    Console.WriteLine($"Индекс элемента в массиве: {pos}");
+}
+else Console.WriteLine("Элемент не найден в массиве");
+Console.WriteLine("-------");
